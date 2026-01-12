7c7697ddc9777cafa2314ccff1fc6ef2bace8362
Phem Dev <phemdev2@gmail.com>
2026-01-12 19:07:00 +0100

    Fix: correct entry point extension from .tsx to .jsx

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>POS Dashboard</title>
     @viteReactRefresh
-    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    @vite(['resources/css/app.css', 'resources/js/main.jsx'])
 </head>
 <body class="bg-gray-100 text-gray-800">
     @inertia
